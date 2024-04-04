898d8fc674dac3c1ddd58fd1e943ec2b062a84ef
Cesar Zanellato <cesarlogrc@gmail.com>
2024-04-03 22:10:10 -0300

    Instalação Class Validator e aplicando no ShelterInput

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumberString, IsPhoneNumber, IsString, Length } from 'class-validator';
+
 export default class UpdateShelterControllerInput {
+  @IsString()
+  @IsNotEmpty()
   name: string;
-  whatsApp: string;
-  email: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 11)
+  whatsapp: string;
+  @IsNotEmpty()
+  @IsNumberString()
   phone: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
