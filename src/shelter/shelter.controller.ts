d2d8a27343f2f90a6915ef60caeb0f4ddb3f47ce
Cesar Zanellato <cesarlogrc@gmail.com>
2024-04-05 22:30:44 -0300

    conexão com banco, criação dos updates (input e output)

--- src/shelter/shelter.controller.ts
+++ src/shelter/shelter.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Get, Inject, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Put,
+} from '@nestjs/common';
 import GetShelterDetailsUseCaseOutput from './usecases/dtos/get.shelter.details.usecase.output';
 import { IUseCase } from 'src/domain/iusecase.interface';
 import ShelterTokens from './shelter.tokens';
 import UpdateShelterControllerInput from './dtos/update.shelter.controller.input';
+import UpdateShelterDetailsUseCaseInput from './usecases/dtos/update.shelter.details.usecase.input';
+import UpdateShelterDetailsUseCaseOutput from './usecases/dtos/update.shelter.details.usecase.output';
 
 @Controller('shelter')
 export class ShelterController {
@@ -12,13 +20,20 @@ export class ShelterController {
     GetShelterDetailsUseCaseOutput
   >;
 
+  @Inject(ShelterTokens.updateShelterDetailsUseCase)
+  private readonly updateShelterDetailsUseCase: IUseCase<
+    UpdateShelterDetailsUseCaseInput,
+    UpdateShelterDetailsUseCaseOutput
+  >;
+
   @Get()
   async getShelterDetails(): Promise<GetShelterDetailsUseCaseOutput> {
     return await this.getShelterDetailsUseCase.run(null);
   }
 
-  @Patch()
-  async updateShelterDatails(@Body() input: UpdateShelterControllerInput) {
-    console.log(input)
+  @Put()
+  async updateShelterDatails(@Body() input: UpdateShelterControllerInput): Promise<UpdateShelterDetailsUseCaseOutput> {
+    const usecaseInput = new UpdateShelterDetailsUseCaseInput({ ...input });
+    return await this.updateShelterDetailsUseCase.run(usecaseInput);
   }
 }
--- src/shelter/shelter.repository.ts
+++ src/shelter/shelter.repository.ts
@@ -1,9 +1,10 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Shelter } from './schemas/shelter.schema';
 import { Model } from 'mongoose';
-import { promises } from 'dns';
 import IShelterRepository from './interfaces/shelter.repository.interface';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class ShelterRepository implements IShelterRepository{
   constructor(
     @InjectModel(Shelter.name)
